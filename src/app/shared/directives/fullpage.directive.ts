01ea87a8a04ed89e94ea5d6c8acb1737bddad613
Angel Newton <xanewton@gmail.com>
2020-02-25 09:57:55 -0500

    Resize the image

--- src/app/shared/directives/fullpage.directive.ts
+++ src/app/shared/directives/fullpage.directive.ts
@@ -1,10 +1,34 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appFullpage]'
 })
 export class FullpageDirective {
 
-  constructor() { }
+  constructor(
+    private element: ElementRef
+  ) { }
+
+  resize() {
+    let bgwidth = this.element.nativeElement.width;
+    let bgheight = this.element.nativeElement.height;
+
+    let winwidth = window.innerWidth;
+    let winheight = window.innerHeight;
+
+    let widthratio = winwidth / bgwidth;
+    let heightratio = winheight / bgheight;
+
+    let widthdiff = heightratio * bgwidth;
+    let heightdiff = widthratio * bgheight;
+
+    if (heightdiff > winheight) {
+      this.element.nativeElement.width = winwidth;
+      this.element.nativeElement.height = heightdiff;
+    } else {
+      this.element.nativeElement.width = widthdiff;
+      this.element.nativeElement.height = winheight;
+    }
+  }
 
 }
